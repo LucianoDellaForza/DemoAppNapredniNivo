92e233f0b1913ab2344ed569f859e194a96b76ab
Luka Krivacevic <47541885+LucianoDellaForza@users.noreply.github.com>
2021-05-24 15:53:27 +0200

    Image loading fix - Picasso lib instead of Glide

--- app/src/main/java/rs/gecko/demoappnapredninivo/ui/adapters/PhotoRecyclerAdapter.kt
+++ app/src/main/java/rs/gecko/demoappnapredninivo/ui/adapters/PhotoRecyclerAdapter.kt
@@ -1,11 +1,11 @@
 package rs.gecko.demoappnapredninivo.ui.adapters
 
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.item_album.view.*
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_photo.view.*
 import rs.gecko.demoappnapredninivo.R
 import rs.gecko.demoappnapredninivo.ui.models.Photo
@@ -32,12 +32,43 @@ class PhotoRecyclerAdapter(
         val photo = albumPhotos[position]
         holder.itemView.apply {
 //            Glide.with(context)
-//                .load(photo.url)
-//                .placeholder(R.drawable.placeholder_pic)
+//                .load(test)
+////                .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+//                .error(R.drawable.placeholder_pic)
+//                .listener(object: RequestListener<Drawable> {
+//                    override fun onLoadFailed(
+//                        e: GlideException?,
+//                        model: Any?,
+//                        target: Target<Drawable>?,
+//                        isFirstResource: Boolean
+//                    ): Boolean {
+//                        Log.d("PhotoRecyclerAdapter", e?.message.toString())
+//                        return false
+//                    }
+//
+//                    override fun onResourceReady(
+//                        resource: Drawable?,
+//                        model: Any?,
+//                        target: Target<Drawable>?,
+//                        dataSource: DataSource?,
+//                        isFirstResource: Boolean
+//                    ): Boolean {
+//                       Log.d("PhotoRecyclerAdapter", "Glide: Success")
+//                        return false
+//                    }
+//
+//                })
+////                .placeholder(R.drawable.placeholder_pic)
 //                .into(photoIv)
+
+            Picasso.get().load(photo.url).placeholder(R.drawable.placeholder_pic).into(photoIv)
+
             photoIdTv.text = photo.id.toString()
         }
+
+
     }
+
 
     override fun getItemCount(): Int {
         return albumPhotos.size
